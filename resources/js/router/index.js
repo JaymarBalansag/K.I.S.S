2b16a34967458391e39c2bb8d1c013dd0bc68e60
Jaymar Balansag <balansagjm@gmail.com>
2026-02-04 09:45:40 +0800

    MarriageLicense-Static

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,7 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 const routes = [
     {
         path: '/', redirect: '/home',
+    },
+    {
+      path: '/isCohabiting', name: 'isCohabiting', component: () => import("../pages/main/MarriageLicense/isCohabiting.vue")  
+    },
+    {
+      path: '/isForeigner/Cohab/:isCohabiting', name: 'IsForeigner', component: () => import("../pages/main/MarriageLicense/isForeigner.vue")
+    },
+    {
+      path: '/ageRange/isForeigner/:isForeigner/Cohab/:isCohabiting', name: 'AgeRange', component: () => import("../pages/main/MarriageLicense/AgeRange.vue")
+    },
+    {
+      path: '/Marriage-Form/ageRange/requirements/:requirements/isForeigner/:isForeigner/Cohab/:isCohabiting', name: 'MarriageForm', component: () => import("../pages/main/MarriageLicense/MarriageForm.vue")
     },
     { 
         path: '/home', name: 'Home', component: () => import('../pages/main/LandingPage.vue') 
